7a12b29da84b375c9a3af22dbac2ebf8472caf7f
HanhND <hanhndth1808055@fpt.edu.vn>
2019-07-24 13:55:24 +0700

    Exercise 1: Find the element in Finbonacci sequence with the given order number

---
+++ ex1190722inclass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex1190722inclass
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> intFibo = new List<int>();
+            Console.WriteLine("Please insert the order number of the Fibonacci chain that you want to find: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            intFibo.Add(1);
+            intFibo.Add(1);
+            for (int i = 2; i < x; i++)
+            {
+                intFibo.Add(intFibo[i - 2] + intFibo[i - 1]);
+            }
+
+            Console.WriteLine(intFibo[x - 1]);
+        }
+    }
+}
