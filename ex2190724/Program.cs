2584636b5c518914c02621c5c8b28344232e92dd
HanhND <hanhndth1808055@fpt.edu.vn>
2019-07-25 19:18:38 +0700

    Assignment 2

--- ex2190724/Program.cs
+++ ex2190724/Program.cs
@@ -4,23 +4,29 @@
     {
         public static void Main(string[] args)
         {
-            Product p = new Product(1, "Oppo mobile", 60, 2, "Imagelink1", "Description mobile.");
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.addPicture();
-            p.getInfo();
-            Product p1 = new Product(1, "Samsung mobile", 80, 0, "Imagelink2", "Description mobile 2.");
-            p.stockStatus();
-            p1.stockStatus();
-            p.removePicture();
+            Cart cart = new Cart(1, "Nguyen Doan Hanh", "Hanoi", "Vietnam");
+            cart.addProduct();
+            cart.addProduct();
+            cart.addProduct();
+            cart.removeProduct();
+            cart.calculateGrandTotal();
+//            Product p = new Product(1, "Oppo mobile", 60, 2, "Imagelink1", "Description mobile.");
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.addPicture();
+//            p.getInfo();
+//            Product p1 = new Product(1, "Samsung mobile", 80, 0, "Imagelink2", "Description mobile 2.");
+//            p.stockStatus();
+//            p1.stockStatus();
+//            p.removePicture();
         }
     }
 }
